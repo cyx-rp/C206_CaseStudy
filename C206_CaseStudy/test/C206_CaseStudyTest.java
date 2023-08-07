abe5633dd5c7006da4052b249dc4842b3c2a3e78
AdrielAcelainea <22016845@myrp.edu.sg>
2023-08-08 01:16:25 +0800

    Update

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,13 +1,31 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	private ServiceProviders sp1;
+	private ServiceProviders sp2;
+	private ServiceProviders sp3;
+	
+	private ArrayList<ServiceProviders> serviceProvidersList;
 
 	@Before
 	public void setUp() throws Exception {
+		sp1 = new ServiceProviders("SP001","Aces Renovation",80);
+		sp2 = new ServiceProviders("SP002","Stellar Services",90);
+		sp3 = new ServiceProviders("SP003","Tucker's",50);
+		serviceProvidersList = new ArrayList<ServiceProviders>();
+		
+		
+	}
+	
+	@Test
+	public void testAddServiceProvider() {
+		
 	}
 
 	@After
